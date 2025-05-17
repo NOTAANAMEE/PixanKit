3d512b0bd866d4a86694103b65d8dd9f4dc4bdc4
ry 徐 <xry070226@gmail.com>
2025-05-17 01:11:29 -0400

    updating

---
+++ Pixankit/LaunchCore/GameModule/Game/GameSettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixanKit.LaunchCore.GameModule.Game;
+
+public struct GameSettings()
+{
+    public readonly bool IsOverall = false;
+
+    public SettingValue JavaSetting;
+
+    public SettingValue RunningFolderSetting;
+
+    public SettingValue JVMArgSetting;
+
+    public SettingValue PreArgSetting;
+
+    public SettingValue PostArgSetting;
+
+    public string CustomJavaSetting;    
+
+    public string CustomRunningFolderSetting;
+
+    public string Description;
+
+    public string CustomJVMArgSetting;
+    
+    public string CustomPreArgSetting;
+
+    public string CustomPostArgSetting;
+
+
+}
+
+public enum SettingValue
+{
+    Overall,
+    Custom,
+    
+    Self,
+
+    Latest,
+    Closest,
+    Specified
+}
